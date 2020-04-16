db445ec06e560d76370aa6bd3a95fa9f24d75d80
moti <moti.kor@gmail.com>
2020-04-16 11:06:32 +0200

    refactor: fun comp

--- src/App.js
+++ src/App.js
@@ -1,46 +1,54 @@
 // The <App /> component is responsible for rendering the two main columns
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BaseStyles } from "@primer/components";
 import { Flex } from "@primer/components";
 import PokemonList from "./columns/PokemonList";
 import PokemonDetails from "./columns/PokemonDetails";
 
-class App extends React.Component {
-  state = {
-    selectedPokemon: null
-  };
-
-  setSelectedPokemon = name => {
-    this.setState({
-      selectedPokemon: name
-    });
-  };
-
-  componentDidMount() {
-    this.updateDocumentTitle();
-  }
-
-  componentDidUpdate(_, prevState) {
-    if (prevState.selectedPokemon !== this.state.selectedPokemon) {
-      this.updateDocumentTitle();
-    }
-  }
-
-  updateDocumentTitle() {
-    const { selectedPokemon } = this.state;
-    document.title = `${selectedPokemon ? `${selectedPokemon} | ` : ""}Pokedex`;
-  }
-
-  render() {
-    return (
-      <BaseStyles>
-        <Flex>
-          <PokemonList setSelectedPokemon={this.setSelectedPokemon} />
-          <PokemonDetails name={this.state.selectedPokemon} />
-        </Flex>
-      </BaseStyles>
-    );
-  }
+function App() {
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  useEffect(() => {
+    document.title = `${
+      selectedPokemon ? `${selectedPokemon} | ` : ""
+    } My Pokedex`;
+  }, [selectedPokemon]);
+  return (
+    <BaseStyles>
+      <Flex>
+        <PokemonList setSelectedPokemon={setSelectedPokemon} />
+        <PokemonDetails name={selectedPokemon} />
+      </Flex>
+    </BaseStyles>
+  );
 }
+
+// class App extends React.Component {
+
+//   componentDidMount() {
+//     this.updateDocumentTitle();
+//   }
+
+//   componentDidUpdate(_, prevState) {
+//     if (prevState.selectedPokemon !== this.state.selectedPokemon) {
+//       this.updateDocumentTitle();
+//     }
+//   }
+
+//   updateDocumentTitle() {
+//     const { selectedPokemon } = this.state;
+//     document.title = `${selectedPokemon ? `${selectedPokemon} | ` : ""}Pokedex`;
+//   }
+
+//   render() {
+//     return (
+//       <BaseStyles>
+//         <Flex>
+//           <PokemonList setSelectedPokemon={this.setSelectedPokemon} />
+//           <PokemonDetails name={this.state.selectedPokemon} />
+//         </Flex>
+//       </BaseStyles>
+//     );
+//   }
+// }
 
 export default App;
